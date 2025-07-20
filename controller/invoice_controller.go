d88109748efeab7ee7d6ca5ad811bfb656443e9e
Imam Fahrudin <fakhim.ilm@gmail.com>
2025-07-20 22:21:11 +0700

    Memperbaharui controller

--- controller/invoice_controller.go
+++ controller/invoice_controller.go
@@ -1,1 +1,13 @@
 package controller
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func GetInvoice(w http.ResponseWriter, r *http.Request) {
+	invoices := []map[string]interface{}{
+		{"id": 1, "title": "Invoice A", "amount": 100},
+	}
+	json.NewEncoder(w).Encode(invoices)
+}
