68726bec9b71b5c19cdcb14bd60f497721f240e3
Imam Fahrudin <fakhim.ilm@gmail.com>
2025-07-20 21:52:36 +0700

    memperbaharui routes

--- routes/router.go
+++ routes/router.go
@@ -1,1 +1,21 @@
 package routes
+
+import (
+	"github.com/fakh1m/go-auth-invoice/controller"
+	"github.com/fakh1m/go-auth-invoice/middleware"
+	"github.com/gorilla/mux"
+)
+
+func SetupRouter() *mux.Router {
+	r := mux.NewRouter()
+
+	r.HandleFunc("api/register", controller.Register).Methods("POST")
+	r.HandleFunc("/api/login", controller.Login).Methods("POST")
+
+	invoiceRouter := r.PathPrefix("/api/invoices").Subrouter()
+	invoiceRouter.Use(middleware.JWTMiddleware)
+	invoiceRouter.HandleFunc("", controller.GetInvoices).Methods("GET")
+
+	return r
+
+}
