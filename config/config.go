a8cac5552015e3e0aed980997d8f00668cdaeeeb
Imam Fahrudin <fakhim.ilm@gmail.com>
2025-07-20 21:49:13 +0700

    memperbaharui config.go

--- config/config.go
+++ config/config.go
@@ -1,1 +1,20 @@
 package config
+
+import (
+	"database/sql"
+	"log"
+)
+
+var DB *sql.DB
+
+func InitDB(){
+	var err error
+	connStr := "user=postgres password=postgres dbname=go_invoice sslmode=disable"
+	DB, err= sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal("Database connection error: ", err)
+	}
+	if err = DB.Ping(); err != nil {
+		log.Fatal("Database ping error: ", err)
+	}
+}
